36d8928ff1ccb393d02f2a59fe051e8436d08512
Sandhanakrishnan Ravichandran <sandhanakrishnan.ravichandran@tum.de>
2024-04-27 13:22:32 +0200

    feature/multiclasslabel

--- dataprocess.py
+++ dataprocess.py
@@ -15,7 +15,7 @@ def fetch_data(session, query):
         data.append({"subject_id": subject_id,"pheno_type":phenotype,"protien":protein, "disease": disease_status})
     return pd.DataFrame(data)
 
-def main():
+def main(type=1):
 
     uri = "bolt://83.229.84.12:7687"  
     username = "tumaiReadonly"
@@ -25,25 +25,43 @@ def main():
 
     with GraphDatabase.driver(uri, auth=AUTH) as driver:
         driver.verify_connectivity()
-        
-
-
-
-    query ="""
-MATCH (b:Biological_sample)
-OPTIONAL MATCH (b)-[:HAS_PROTEIN]->(p:Protein)
-OPTIONAL MATCH (b)-[:HAS_PHENOTYPE]->(ph:Phenotype)
-OPTIONAL MATCH (b)-[:HAS_DISEASE]->(d:Disease)
-RETURN b.subjectid AS subject_id, 
-       collect(DISTINCT p.id) AS proteins,
-       collect(DISTINCT ph.id) AS phenotypes,
-       CASE WHEN d.name = 'control' THEN 0 ELSE 1 END AS disease
+    if type==0:
+        query ="""
+    MATCH (b:Biological_sample)
+    OPTIONAL MATCH (b)-[:HAS_PROTEIN]->(p:Protein)
+    OPTIONAL MATCH (b)-[:HAS_PHENOTYPE]->(ph:Phenotype)
+    OPTIONAL MATCH (b)-[:HAS_DISEASE]->(d:Disease)
+    RETURN b.subjectid AS subject_id, 
+        collect(DISTINCT p.id) AS proteins,
+        collect(DISTINCT ph.id) AS phenotypes,
+        CASE WHEN d.name = 'control' THEN 0 ELSE 1 END AS disease
+        """
+    if type==1:
+        query = """
+        MATCH (b:Biological_sample)-[:HAS_DISEASE]->(d:Disease)
+        WHERE NOT d.name = 'control'
+        OPTIONAL MATCH (b)-[:HAS_PROTEIN]->(p:Protein)
+        OPTIONAL MATCH (b)-[:HAS_PHENOTYPE]->(ph:Phenotype)
+        WITH b, 
+            collect(DISTINCT p.id) AS proteins,
+            collect(DISTINCT ph.id) AS phenotypes,
+            d.synonyms AS synonyms
+        UNWIND synonyms AS synonym
+        WITH b, proteins, phenotypes, synonym
+        WHERE synonym CONTAINS 'ICD10CM:'
+        RETURN b.subjectid AS subject_id, 
+            proteins,
+            phenotypes,
+            substring(synonym, size('ICD10CM:'), 1) AS disease
     """
-
     with driver.session(database=database) as session:
         data = fetch_data(session, query)
         
     return data
+
+
+
+
 
 def process_data(data):
     mlb_pheno = MultiLabelBinarizer()
---
+++ train.py
@@ -0,0 +1,41 @@
+
+from dataprocess import *
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+type = 1
+data = main(type=type)
+df = process_data(data)
+# df.head()
+
+
+X = df.drop(['subject_id', 'disease'], axis=1)
+y = df['disease']
+if type==1:
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
+
+classifier = RandomForestClassifier(n_estimators=3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+classifier.fit(X_train, y_train)
+
+
+# Predict the test set
+y_pred = classifier.predict(X_test)
+results_df = pd.DataFrame({
+    'subject_id': X_test.index,  
+    'disease': y_pred
+})
+if type==0:
+    results_df.to_csv('taskA_predictions.csv')
+else:
+    results_df.to_csv('taskB_predictions.csv')
+# print(classification_report(y_test, y_pred))
+# print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
